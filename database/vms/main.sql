b413d73c769e1e844e5502934e6ff39bbef14188
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2026-04-10 23:22:31 +0300

    feat: add points_transactions, events, event_tickets, and event_registrations tables to database schema

--- database/vms/main.sql
+++ database/vms/main.sql
@@ -32,4 +32,54 @@ CREATE TABLE IF NOT EXISTS tasks (
     completed_at TEXT, -- stored as ISO 8601 string (e.g., "1990-01-01T12:00:00Z")
     -- completion_approval_status TEXT NOT NULL DEFAULT 'pending' -- "pending", "approved", "rejected"
     completion_approval_by TEXT -- membership_number of the user who approved the task completion (set when approved, rejection will reset the completed_by and completed_at fields to NULL)
-)
+);
+
+CREATE TABLE IF NOT EXISTS points_transactions (
+    id TEXT PRIMARY KEY,
+    created_at TEXT NOT NULL DEFAULT (datetime('now')),
+    updated_at TEXT NOT NULL DEFAULT (datetime('now')) ON UPDATE CASCADE,
+    membership_number TEXT NOT NULL, -- membership_number of the user whose points are being changed
+    task_id TEXT, -- task_id associated with the points change (can be NULL for non-task-related transactions)
+    amount INTEGER NOT NULL,
+    type TEXT NOT NULL, -- "task_reward", "purchase", "event_attendance", "other"
+);
+
+CREATE TABLE IF NOT EXISTS events (
+    id TEXT PRIMARY KEY,
+    created_at TEXT NOT NULL DEFAULT (datetime('now')),
+    updated_at TEXT NOT NULL DEFAULT (datetime('now')) ON UPDATE CASCADE,
+    name TEXT NOT NULL,
+    description TEXT,
+    start_time TEXT NOT NULL, -- stored as ISO 8601 string (e.g., "1990-01-01T12:00:00Z")
+    end_time TEXT NOT NULL, -- stored as ISO 8601 string (e.g., "1990-01-01T12:00:00Z")
+    location TEXT,
+    created_by TEXT NOT NULL -- membership_number of the user who created the event
+    project_id TEXT REFERENCES projects(id), -- optional association with a project
+    required_skills TEXT, -- comma-separated list of skills required for the event (e.g., "python,project_management,design")
+    recommended_skills TEXT, -- comma-separated list of skills recommended for the event (e.g., "python,project_management,design")
+    aquired_skills TEXT -- comma-separated list of skills that participants can acquire or improve by attending the event (e.g., "python,project_management,design")
+);
+
+CREATE TABLE IF NOT EXISTS event_tickets ( -- the available tickets for the events, 
+    id TEXT PRIMARY KEY,
+    created_at TEXT NOT NULL DEFAULT (datetime('now')),
+    updated_at TEXT NOT NULL DEFAULT (datetime('now')) ON UPDATE CASCADE,
+    event_id TEXT NOT NULL REFERENCES events(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    name TEXT NOT NULL,
+    description TEXT,
+    point_price INTEGER NOT NULL, -- price in points (can be 0 for free tickets, negative for rewarding points to participants, and positive for charging points from participants)
+    currency_price TEXT NOT NULL, -- price in currency (e.g., 10 $USD, 250 TRY, 10000 SYP, etc.)
+    quantity INTEGER NOT NULL -- total quantity of this ticket type available for the event
+);
+
+CREATE TABLE IF NOT EXISTS event_registrations ( -- the registrations of users to the events, each registration is for one ticket
+    id TEXT PRIMARY KEY,
+    created_at TEXT NOT NULL DEFAULT (datetime('now')),
+    updated_at TEXT NOT NULL DEFAULT (datetime('now')) ON UPDATE CASCADE,
+    event_id TEXT NOT NULL REFERENCES events(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    membership_number TEXT NOT NULL, -- membership_number of the user who registered for the event
+    ticket_id TEXT NOT NULL REFERENCES event_tickets(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    status TEXT NOT NULL, -- "registered", "attended", "cancelled", "no_show"
+    approved_by TEXT -- membership_number of the user who approved the status (e.g., marking as attended, marking as no_show etc.)
+);
+
