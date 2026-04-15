4393e72afb309c0998e8e8ccf34df318a4ba0aa4
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2026-04-15 23:39:44 +0300

    feat: add attendance approval and primary key constraint to event_registrations table

--- database/vms/main.sql
+++ database/vms/main.sql
@@ -117,7 +117,8 @@ CREATE TABLE IF NOT EXISTS event_registrations ( -- the registrations of users t
     ticket_id TEXT NOT NULL REFERENCES event_tickets(id) ON DELETE CASCADE,
     status TEXT NOT NULL, -- "registered", "attended", "cancelled", "no_show"
     payment_approved_by TEXT, -- membership_number of the user who approved the payment (e.g., "1234567890")
-    attendance_approved_by TEXT -- membership_number of the user who approved the attendance status (e.g., marking as attended, marking as no_show etc.)
+    attendance_approved_by TEXT. -- membership_number of the user who approved the attendance status (e.g., marking as attended, marking as no_show etc.)
+    PRIMARY KEY (event_id, membership_number) -- a user can only register once for an event, but can have multiple registrations for different events. TODO: check that the app doesn't run into issues
 );
 
 CREATE TRIGGER IF NOT EXISTS update_event_registration_updated_at AFTER UPDATE ON event_registrations
